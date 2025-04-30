851d48ed3c5cb4db9f0adecccc0997d90b200e20
ShivaniDixit05 <shivanidixit0525@gmail.com>
2025-04-30 09:42:38 +0530

    Create comments.py

---
+++ comments.py
@@ -0,0 +1,15 @@
+# \n is a escape sequence character / new line character
+
+print("Hey I am a good girl \n and my friend also a good girl") # new line character
+
+'''
+
+this is shivani
+i want to learn python    
+
+'''
+
+print("hello guyss , this is \"shivani\"\n and i am a second year student") # double code escape seequence character \" (back slash)
+
+print("hey" , 6 , 4 , sep = "~" , end = "05")
+print("guyz")
