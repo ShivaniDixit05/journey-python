5e24b2ae5048f6f997487438a81318e65f1a8abf
ShivaniDixit05 <shivanidixit0525@gmail.com>
2025-05-01 23:33:14 +0530

    Add files via upload

---
+++ variable.py
@@ -0,0 +1,29 @@
+a = 1
+a1 = 9
+b = "Hello"
+c = True                        # Data types
+d = None
+e = 2.45
+f = complex(4 , 6)
+
+print(a)
+print(b)
+print(f)
+print(a + a1)
+
+print("The type is :" , type(a))
+print("The type is :" , type(a1))
+print("The type is :" , type(b))
+print("The type is :" , type(c))
+print("The type is :" , type(d))
+print("The type is :" , type(e))
+print("The type is :" , type(f))
+
+list1 = [2 , 3.5 , [-5 , +5] , ["watermelon" , "apple"]]
+print(list1)
+
+tuple1 = (("s" , "s") , ("p" , "p"))
+print(tuple1)
+
+dict1 = {"name":"Siya" , "age":19 , "canVote":True}  
+print(dict1)
