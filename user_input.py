f44dc9da79b5988b91d0b25cd4fa4bf0fe60dcf5
ShivaniDixit05 <shivanidixit0525@gmail.com>
2025-05-04 12:11:34 +0530

    Create user_input.py

---
+++ user_input.py
@@ -0,0 +1,10 @@
+# a = input()
+# print("My name is ",a)
+
+a = input("Enter your name :")
+print("My name is ",a)
+
+x = input("Enter first number :")
+y = input("Enter second number :")
+print(x + y)    #join bcz of str
+print(int(x) + int(y)) #sum bcz of typecating
