71f9cda09b2f9d96d839fc6ee8f1710dfbfc4999
ShivaniDixit05 <shivanidixit0525@gmail.com>
2025-05-05 14:06:59 +0530

    Create strings.py

---
+++ strings.py
@@ -0,0 +1,28 @@
+name = "siya"
+print("Hello, " + name) 
+
+# apple = "He said, \"I want to eat an apple"  OR
+apple = 'He said , "I want to eat apple".'
+print(apple)
+
+# #more lines :- we can use both single or double quotes
+
+lines = '''He said,
+Hi Siya , How are you ?
+Hey I am good
+"I want to eat an apple"'''
+print(lines)
+
+Person = "Shivani"
+print(Person[0])
+print(Person[1])
+print(Person[2])
+print(Person[3])
+print(Person[4])
+print(Person[5])
+print(Person[6])
+# # print(Person[7])  # Throws an index error
+
+print("Let's use a for loop \n")
+for character in lines:
+    print(character)
