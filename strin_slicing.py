db418b34c470bfd492fd9ea12d3978f37fabe93c
ShivaniDixit05 <shivanidixit0525@gmail.com>
2025-05-07 01:30:46 -0600

    Create strin_slicing.py

---
+++ strin_slicing.py
@@ -0,0 +1,17 @@
+fruit = "Mango"
+len1 = len(fruit)
+print("The fruit Mango have" , len1 , "letters")
+
+veggie = "Aaloo"
+print(len(veggie))
+print(veggie[0:4])    # including 0 but not 4                0 to N-1
+print(veggie[:4])     # automatically including 0 
+print(veggie[1:4])    # including 1 but not 4
+print(veggie[0:-2])   # print(veggie[0:len of veggie -2])
+print(veggie[-1:-3])
+# print(veggie[-3:-1]) 
+
+
+# Quick Quiz:--
+nm = "Harry"
+print(nm[-4:-2]) 
