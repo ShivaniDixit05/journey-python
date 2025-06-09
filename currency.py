3473ff74f092c3cfc6453486e8fdfc07597ef088
ShivaniDixit05 <shivanidixit0525@gmail.com>
2025-06-09 11:26:37 +0530

    Create currency.py

---
+++ currency.py
@@ -0,0 +1,19 @@
+# Write code below 💖
+
+COP = int(input("what do you have left in pesos ?"))
+ExchangeRate = 4176.57 
+USD1 = COP/ExchangeRate
+print(USD1)
+
+PEN = int(input("What do you have left in soles ?"))
+ExchangeRate = 0.28 
+USD2 = PEN/ExchangeRate
+print(USD2)
+
+BRL= int(input("What do you have left in reais ?"))
+ExchangeRate = 0.18
+USD3 =  BRL * ExchangeRate
+print(USD3)
+
+Sum = USD1 + USD2 + USD3
+print(Sum)
