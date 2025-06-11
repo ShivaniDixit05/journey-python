27e4f644959d0bd4b7bed784453532ab19bb6950
ShivaniDixit05 <shivanidixit0525@gmail.com>
2025-06-11 22:59:08 +0530

    Create season.py

---
+++ season.py
@@ -0,0 +1,14 @@
+# Write code below 💖
+
+month = int(input("Enter the number foe checking the season : "))
+
+if month > 0 and month <= 3:
+  print("Winter 🌨️")
+elif month > 3 and month <= 6:
+  print("Spring 🌱")
+elif month > 6 and month <= 9:
+  print("Summer 🌻")
+elif month > 9 and month <= 12:
+  print("Autumn 🍂") 
+else:
+  print("Invalid")  
