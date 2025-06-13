ef7d4428b0bd7151ec0d0673e38798a32a1dad9a
ShivaniDixit05 <shivanidixit0525@gmail.com>
2025-06-13 11:28:50 +0530

    Create while_loop.py

---
+++ while_loop.py
@@ -0,0 +1,27 @@
+i = 0
+while (i <= 3):
+    print(i)
+    i = i + 1       #increment
+print("Done with the loop")    
+
+i = int(input("Enter the number :"))
+while (i <= 36):
+    i = int(input("Enter the number :"))
+    print(i)
+print("Done with the loop")    
+
+count = 5
+while(count > 0):
+    print(count)          #Decrement
+    count = count - 1    #count = count + 1    it becomes an infinite loop
+
+x = 8
+while(x > 0):
+    print(x)
+    x = x - 1
+else:
+    print("I am inside the else")    
+
+# do{
+#     # Loop Body
+# }while(comdition);
