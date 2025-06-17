6e1583675494f62f3bbf289f83fb9fc7253df7b2
ShivaniDixit05 <shivanidixit0525@gmail.com>
2025-06-17 20:20:34 +0530

    Create for_loop.py

---
+++ for_loop.py
@@ -0,0 +1,37 @@
+name = 'Shivani'
+for i in name:
+    print(i)
+    if (i == "v"):
+        print("This is something special")
+
+colours = ["Red" , "Blue" , "yellow" , "Green"]
+for char in colours:
+    print(char)
+    for i in char:
+        print(i)
+
+for k in range(5):
+    print(k)
+
+for k in range(1 , 20000):       #range(start , stop)
+    print(k + 1)
+
+for k in range(1 , 20 , 2):      #range(start , stop , step)
+    print(k + 1)
+
+for k in range(1 , 12 , 3):      #range(start , stop , step)
+    print(k)
+
+
+for k in range(1):
+    print(k)
+
+
+
+# i = 0
+# while True:
+#   print(i)
+#   i = i + 1
+#   if(i%100 == 0):
+#      break   #loop bbreak after 99
+#      continue  # infinity
