029a5f51d15325c80819cb91e6aa30765781c6d0
ShivaniDixit05 <shivanidixit0525@gmail.com>
2025-07-03 20:50:22 +0530

    Create doc_string.py

---
+++ doc_string.py
@@ -0,0 +1,39 @@
+# ---------: DocString :----------
+
+def square(n):
+    '''Takes in a number n , returns the square of n'''     # Right above the fun name
+    print(n**2)
+square(5)
+print(square.__doc__)      # This command print the doc string
+
+
+def square(n):
+    print(n)
+    '''Takes in a number n , returns the square of n'''  # for this code this is not a doc string 
+    print(n**2)
+square(5)    
+print(square.__doc__)
+
+
+# -----------: Python Enhancement Proposal [PEP] :------------
+# The Zen of Python, by Tim Peters
+
+# Beautiful is better than ugly.
+# Explicit is better than implicit.
+# Simple is better than complex.
+# Complex is better than complicated.
+# Flat is better than nested.
+# Sparse is better than dense.
+# Readability counts.
+# Special cases aren't special enough to break the rules.
+# Although practicality beats purity.
+# Errors should never pass silently.
+# Unless explicitly silenced.
+# In the face of ambiguity, refuse the temptation to guess.
+# There should be one-- and preferably only one --obvious way to do it.
+# Although that way may not be obvious at first unless you're Dutch.
+# Now is better than never.
+# Although never is often better than *right* now.
+# If the implementation is hard to explain, it's a bad idea.
+# If the implementation is easy to explain, it may be a good idea.
+# Namespaces are one honking great idea -- let's do more of those!
