e10649a22f51071d0fac9ec0f190e1c2154b4c64
ShivaniDixit05 <shivanidixit0525@gmail.com>
2025-07-05 13:31:09 +0530

    Create set.py

---
+++ set.py
@@ -0,0 +1,13 @@
+# set are unordered collection of data items
+
+s = {8, 4 , 6 , 7 , 2 , 4 , 3}
+print(s)           # set do not print repeated values
+
+info = {"Siya" , 21 , False , 5.9 , 21}
+print(info)
+
+harry = set()       
+print(type(harry))
+
+for value in info:
+    print(value)
