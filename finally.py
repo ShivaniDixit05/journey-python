662bb4408045b95284f8a8ba2f5102b32716db31
ShivaniDixit05 <shivanidixit0525@gmail.com>
2025-07-13 19:49:53 +0530

    Create finally.py

---
+++ finally.py
@@ -0,0 +1,29 @@
+# finally code is always executes even if there is an error
+
+try :
+    l = [1 , 4 , 5 ,6]
+    i = int(input("Enter the index : "))
+    print(l[i])
+except:
+    print("Some Error occured")
+finally:
+    print("I am always executed")        
+
+# doubt :- we can use only print("I am always executed") execpt finally what's the problem
+# ans :- when we wrap up the code inside the function then it will give us a problem
+
+def fun():
+    try:
+        l = [1 , 4 , 5 , 6]
+        i = int(input("Enter the index : "))
+        print(l[i])  
+        return 1
+    except:
+        print("Some Error occured")
+        return 0    
+    # print("I am always executed") # it will not be executed
+    finally:
+        print("I am always executed")
+
+x = fun()
+print(x)
