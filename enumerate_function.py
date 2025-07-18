9122211409e9aae654d2d1b2fd3c426a4d9db040
ShivaniDixit05 <shivanidixit0525@gmail.com>
2025-07-18 20:37:21 +0530

    Update enumerate_function.py

--- enumerate_function.py
+++ enumerate_function.py
@@ -1,1 +1,10 @@
+# NORMAL CODE :
 
+marks = [12 , 45 , 32 , 98 , 45 , 1 , 4 , 36]
+
+index = 0
+for mark in marks:
+    print(mark)
+    if (index == 3):
+        print("Harry , Awesome!")
+    index += 1    
